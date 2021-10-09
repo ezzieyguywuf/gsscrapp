f42918406eec9136ba6fb6e40b625385c5c36030
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-10-09 13:42:21 -0400

    Create wrapRequest helper and refactor
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,24 +12,18 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double",
-            {
-                "allowTemplateLiterals": true
-            }
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
+        "indent": ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        "quotes": ["error", "double", {"allowTemplateLiterals": true }],
+        "semi": ["error", "always"],
+        "node/exports-style": ["error", "module.exports"],
+        "node/file-extension-in-import": ["error", "always"],
+        "node/prefer-global/buffer": ["error", "always"],
+        "node/prefer-global/console": ["error", "always"],
+        "node/prefer-global/process": ["error", "always"],
+        "node/prefer-global/url-search-params": ["error", "always"],
+        "node/prefer-global/url": ["error", "always"],
+        "node/prefer-promises/dns": "error",
+        "node/prefer-promises/fs": "error"
     }
 };
--- gsscrapp.js
+++ gsscrapp.js
@@ -1,6 +1,8 @@
 import https from 'https';
 import fs from 'fs';
 
+const HOST = `maps.googleapis.com`
+
 function processArgs() {
     const args = process.argv;
 
@@ -14,8 +16,29 @@ function processArgs() {
     }
 }
 
-const key = processArgs();
-const host = `maps.googleapis.com`
+function wrapRequest(options, fn) {
+    return new Promise((resolve, reject) => {
+        const req = https.request(options, res => {
+            let body = "";
+
+            res.on("data", chunk => body += chunk.toString());
+            res.on("error", reject);
+            res.on("end", () => {
+
+                if (res.statusCode > 299) {
+                    const msg = `request failed. status: ${res.statusCode}, body: ${body}`;
+                    console.log(`ERROR!!! ${msg}`)
+                    reject(msg);
+                }
+                else {
+                    fn(body, resolve);
+                }
+            });
+        }); // const req
+        req.on('error', reject);
+        req.end();
+    }) // new promise
+}
 
 function parseResults(results) {
     return results.map( res => {
@@ -28,68 +51,43 @@ function parseResults(results) {
         };
     });
 }
-async function getPlaces(searchString) {
-    // const endpoint = `/maps/api/place/findplacefromtext/json`;
+
+async function getPlaces(searchString, key, nextPage = undefined) {
     const endpoint = `/maps/api/place/textsearch/json`;
-    // const fields = ["formatted_address", "name", "place_id"].join("%2C");
-    // const fields = "place_id";
-    const searchString = searchString.split(" ").join("%20");
-    // const path = `${endpoint}?fields=${fields}&input=${searchString}&inputtype=textquery&key=${key}`
-    const path = `${endpoint}?query=${searchString}&key=${key}`
-    const options = {hostname: host, path: path, method: "GET"}
+    const query = searchString.split(" ").join("%20");
+    const path = `${endpoint}?query=${query}&key=${key}` +
+        (nextPage === undefined ? '' : `&pagetoken=${nextPage}`);
+    const options = {hostname: HOST, path: path, method: "GET"}
 
-    return new Promise((resolve, reject) => {
-        const req = https.request(options, res => {
-            console.log(`options = ${JSON.stringify(options, null, 4)}`)
-            let body = "";
+    console.log(`path = ${path}`)
+    return wrapRequest(options, async (data, resolve) => {
+        const {next_page_token, results} = JSON.parse(data);
+        const parsedResults = parseResults(results);
 
-            res.on("data", chunk => body += chunk.toString());
-            res.on("error", reject);
-            res.on("end", async () => {
-                if (res.statusCode >= 200 && res.statusCode <= 299) {
-                    const {next_page_token, results} = JSON.parse(body);
-                    const parsedResults = parseResults(results);
+        if (next_page_token !== undefined) {
+            // the google api docs say that there's a 'slight delay'
+            // before the next_page_token is valid (they don't say
+            // how long the delay is...)
+            console.log("sleeping...");
+            await new Promise((resolve) => setTimeout(resolve, 3000));
+            console.log("    recursing...");
+            const moreResults = await getPlaces(searchString, key, next_page_token);
+            resolve(parsedResults.concat(parseResults(moreResults)));
+        }
 
-                    if (next_page_token !== undefined) {
-                        // the google api docs say that there's a 'slight delay'
-                        // before the next_page_token is valid (they don't say
-                        // how long the delay is...)
-                        console.log("sleeping...");
-                        await new Promise((resolve) => setTimeout(resolve, 3000));
-                        console.log("    recursing...");
-                        const moreResults = await getPlaces({
-                            ...options,
-                            path: `${path}&pagetoken=${next_page_token}`
-                        });
-                        console.log(`moreResults = ${moreResults}`)
-                        resolve(parsedResults.concat(parseResults(moreResults)));
-                    }
-
-                    resolve(parsedResults);
-                }
-                else {
-                    const msg = `request failed. status: ${res.statusCode}, body: ${body}`;
-                    console.log(`ERROR!!! ${msg}`)
-                    reject(msg);
-                }
-            }) // res.on("end"...)
-        }); // const req
-        req.on('error', error => {
-            console.log(`THERE WAS AN ERROR`);
-            return reject("there was some other kind of error");
-        });
-        req.end();
-    }) // new promise
+        resolve(parsedResults);
+    }); 
 };
 
-// console.log(`request: ${path}`)
-// const data = await getPlaces("Board Game Stores in Raleigh, North Carolina");
-// console.log(`here's the data I got`)
+const key = processArgs();
+
+const data = await getPlaces("Board Game Stores in Raleigh, North Carolina", key);
+console.log(`here's the data I got`)
 // console.log(data)
-// console.log(`writing to nc_data.json`);
+console.log(`writing to nc_data.json`);
 
-// fs.writeFileSync("./nc_data.json", JSON.stringify(data, null, 4), 'utf8');
+fs.writeFileSync("./nc_data.json", JSON.stringify(data, null, 4), 'utf8');
 
-const data = JSON.parse(fs.readFileSync('./nc_data.json', 'utf8'));
+// const data = JSON.parse(fs.readFileSync('./nc_data.json', 'utf8'));
 
 console.log(`got ${data.length} results`)
