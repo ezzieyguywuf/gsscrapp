e9f224fb519a76e8cf1b3262e5a72f159b3a282a
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-10-08 22:28:30 -0400

    Use the Promise api
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- gsscrapp.js
+++ gsscrapp.js
@@ -16,11 +16,33 @@ function processArgs() {
 const key = processArgs();
 const host = `maps.googleapis.com`
 const endpoint = `/maps/api/place/findplacefromtext/json`;
-const fields = ["formatted_address", "name"].join("%2C");
-const searchString = "Board Game Stores".split(" ").join("%20");
+// const endpoint = `/maps/api/place/textsearch/json`;
+// const fields = ["formatted_address", "name", "place_id"].join("%2C");
+const fields = "place_id";
+const searchString = "Board Game Stores in Raleigh, North Carolina".split(" ").join("%20");
 const path = `${endpoint}?fields=${fields}&input=${searchString}&inputtype=textquery&key=${key}`
+// const path = `${endpoint}?query=${searchString}&key=${key}`
 
-console.log(`request: ${path}`)
+async function getData(options) {
+    return new Promise((resolve, reject) => {
+        const req = https.request(options, res => {
+            let body = "";
+
+            res.on("data", chunk => body += chunk.toString());
+            res.on("error", reject);
+            res.on("end", () => {
+                if (res.statusCode >= 200 && res.statusCode <= 299) {
+                    resolve(body);
+                }
+                else {
+                    reject(`request failed. status: ${res.statusCode}, body: ${body}`);
+                }
+            }) // res.on("end"...)
+        }); // const req
+        req.on('error', error => reject);
+        req.end();
+    }) // new promise
+};
 
 const options = {
     hostname: host,
@@ -28,16 +50,6 @@ const options = {
     method: "GET"
 }
 
-const req = https.request(options, res => {
-    console.log(`statusCode: ${res.statusCode}`);
-
-    res.on('data', d => {
-        process.stdout.write(d);
-    })
-})
-
-req.on('error', error => {
-    console.error(error);
-})
-
-req.end();
+console.log(`request: ${path}`)
+const data = await getData(options);
+console.log(data)
