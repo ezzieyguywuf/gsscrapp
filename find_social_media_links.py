0e1873df43125554f0c64ddf1576155776520a24
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-10-10 21:38:18 -0400

    Commit fixes for fetching social media links
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- find_social_media_links.py
+++ find_social_media_links.py
@@ -3,40 +3,53 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+def updateListOfLinks(links, link):
+    """
+    This will modify the links list in-place
+    """
+    href = link.get('href')
+    if href[:4] == "http" and not href in links:
+        links.append(href)
+
 def getLinks(url):
     """
     Will scrape the given url, and return any social media links within
     """
     print("Fetching content from " + url)
-    response = requests.get(url)
-    print("...done fetching, parsing now...")
+    timeout = 5
+    try:
+        response = requests.get(url, timeout=timeout)
+    except requests.exceptions.ReadTimeout:
+        print("    timed out after {} secs".format(timeout))
+        return {"facebook": "timed out", "twitter": "timed out"}
+    except requests.exceptions.SSLError:
+        return {"facebook": "SSL error", "twitter": "SSL error"}
     soup = BeautifulSoup(response.content, 'html.parser')
-        
     facebookLinks = []
     for link in soup.find_all(href=re.compile("facebook")):
-        href = link.get('href')
-        if not href in facebookLinks:
-            facebookLinks.append(href)
+        updateListOfLinks(facebookLinks, link)
 
     twitterLinks = []
     for link in soup.find_all(href=re.compile("twitter")):
-        href = link.get('href')
-        if not href in twitterLinks:
-            twitterLinks.append(href)
+        updateListOfLinks(twitterLinks, link)
 
     if len(facebookLinks) == 1 and len(twitterLinks) == 1:
         return {"facebook": facebookLinks[0], "twitter": twitterLinks[0]}
 
-    print("for url = " + url)
+    # print("for url = " + url)
     if len(facebookLinks) > 1 and len(twitterLinks) == 1:
-        print("    WARNING! more than one facebook link found")
+        # print("    WARNING! more than one facebook link found")
         return {"facebook": ",".join(facebookLinks), "twitter": twitterLinks[0]}
     elif len(facebookLinks) == 1 and len(twitterLinks) > 1:
-        print("    WARNING! more than one twitter link found")
+        # print("    WARNING! more than one twitter link found")
         return {"facebook": facebookLinks[0], "twitter": ",".join(twitterLinks)}
+    elif len(facebookLinks) > 1 and len(twitterLinks) > 1:
+        # print("    WARNING! more than one facebook and twitter link found")
+        return {"facebook": ",".join(facebookLinks), "twitter": ",".join(twitterLinks)}
+    elif len(soup.find_all(id = re.compile("captcha"))) > 0:
+        return {"facebook": "captcha", "twitter": "captcha"}
     else:
-        print("    WARNING! more than one facebook and twitter link found")
-        return {"facebook": ",".join(facebookLinks[0]), "twitter": ",".join(twitterLinks)}
+        return {"facebook": "could not find", "twitter": "could not find"}
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
